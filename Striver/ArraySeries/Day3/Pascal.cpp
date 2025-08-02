609ed449e688c9520979be4e537e3be661519ff2
sabique <sabiqueislam7@gmail.com>
2025-08-02 19:52:03 +0530

    add Pascal's Triangle generation

---
+++ Striver/ArraySeries/Day3/Pascal.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+
+    vector<int> generateRow(int row){
+        long long ans = 1;
+        vector <int> ansRow;
+        ansRow.push_back(1);
+        for(int col=1; col<row; col++){
+            ans = ans *(row-col);
+            ans = ans/col;
+            ansRow.push_back(ans);
+        }
+        return ansRow;
+    }
+
+    vector<vector<int>> generate(int rows) {
+        vector<vector<int>> result;
+        for(int i=1; i<=rows; i++){
+            result.push_back(generateRow(i));
+        }
+        return result;
+    }
+};
